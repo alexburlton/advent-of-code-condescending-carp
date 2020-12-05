4dbb9c87ae3be4059b0f56d19f2fbbd39860b24f
alexburlton <alexburlton@hotmail.com>
2020-12-05 09:29:03 +0000

    TDD'd version using zip :D

--- src/day_5/day_5.py
+++ src/day_5/day_5.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional, Iterator
 
 from utils import read_text_list
 
@@ -26,15 +26,26 @@ def part_a(input_lines: List[str]) -> None:
 
 def part_b(input_lines: List[str]) -> None:
     seat_ids: List[int] = [get_seat_id(seat_str) for seat_str in input_lines]
+    print(find_missing_seat(seat_ids))
+
+
+def find_missing_seat(seat_ids: List[int]) -> int:
     sorted_seats: List[int] = sorted(seat_ids)
 
-    current_seat = 0
-    for seat in sorted_seats:
-        diff = seat - current_seat
-        if diff == 2:
-            print(seat - 1)
+    high_seats = sorted_seats.copy()
+    high_seats.pop(0)
 
-        current_seat = seat
+    zipped_lists: Iterator[tuple[int, int]] = zip(sorted_seats, high_seats)
+
+    missing_seats = [get_missing_seat(t[0], t[1]) for t in zipped_lists]
+    return next(filter(lambda x: x is not None, missing_seats))
+
+
+def get_missing_seat(low_seat: int, high_seat: int) -> Optional[int]:
+    if high_seat - low_seat == 2:
+        return high_seat - 1
+    else:
+        return None
 
 
 if __name__ == '__main__':
--- src/day_5/day_5.test.py
+++ src/day_5/day_5.test.py
@@ -24,6 +24,18 @@ class TestDay5(unittest.TestCase):
         self.assertEqual(get_seat_id('FFFBBBFRRR'), 119)
         self.assertEqual(get_seat_id('BBFFBBFRLL'), 820)
 
+    def test_get_missing_seat(self):
+        self.assertEqual(get_missing_seat(2, 4), 3)
+
+        self.assertEqual(get_missing_seat(2, 5), None)
+        self.assertEqual(get_missing_seat(2, 3), None)
+
+    def test_find_missing_seat(self):
+        self.assertEqual(find_missing_seat([1, 2, 3, 4, 6]), 5)
+        self.assertEqual(find_missing_seat([2, 3, 6, 1, 4]), 5)
+        self.assertEqual(find_missing_seat([1, 4, 7, 9]), 8)
+        self.assertEqual(find_missing_seat([3, 10, 4, 9, 7]), 8)
+
 
 if __name__ == '__main__':
     unittest.main()
