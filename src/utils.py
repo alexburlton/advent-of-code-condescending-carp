8111e1874e3b48eed542e4fecdf2d7c42c2d6443
alexburlton <alexburlton@hotmail.com>
2020-12-11 10:11:21 +0000

    Day 11 utilities

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 from itertools import tee
 from typing import List, TypeVar, Callable, Iterable
 
+Point = tuple[int, int]
+Grid = dict[Point, str]
+
 
 def read_integer_list(file_name: str) -> List[int]:
     f = open(file_name, "r")
@@ -14,6 +17,34 @@ def read_text_list(file_name: str) -> List[str]:
     ret = list(f.read().splitlines())
     f.close()
     return ret
+
+
+def get_adjacents(grid: Grid, coord: Point) -> List[str]:
+    adjacent_coords = [add_points(coord, d) for d in get_unit_directions()]
+    return [grid.get(adj, None) for adj in adjacent_coords if grid.get(adj, None) is not None]
+
+
+def get_unit_directions() -> List[Point]:
+    return [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+
+def add_points(a: Point, b: Point) -> Point:
+    return a[0] + b[0], a[1] + b[1]
+
+
+def read_coordinate_grid(file_name: str) -> Grid:
+    text_list: List[str] = read_text_list(file_name)
+    return parse_coordinate_grid(text_list)
+
+
+def parse_coordinate_grid(rows: List[str]) -> Grid:
+    grid = {}
+    for y in range(0, len(rows)):
+        row = rows[y]
+        coords = {(i, y): value for i, value in enumerate(row)}
+        grid.update(coords)
+
+    return grid
 
 
 def windowed(iterable, size):
--- src/utils.test.py
+++ src/utils.test.py
@@ -21,6 +21,37 @@ class TestUtils(unittest.TestCase):
         result = windowed([1, 3, 6, 10, 15], 3)
         self.assertEqual(result, [(1, 3, 6), (3, 6, 10), (6, 10, 15)])
 
+    def test_read_coordinate_grid(self):
+        input_lines = ['#.', '@@', '..']
+        grid = parse_coordinate_grid(input_lines)
+        self.assertEqual(grid[(0, 0)], '#')
+        self.assertEqual(grid[(1, 0)], '.')
+        self.assertEqual(grid[(0, 1)], '@')
+        self.assertEqual(grid[(1, 1)], '@')
+        self.assertEqual(grid[(0, 2)], '.')
+        self.assertEqual(grid[(1, 2)], '.')
+
+    def test_add_points(self):
+        self.assertEqual(add_points((0, 1), (5, 10)), (5, 11))
+
+    def test_get_adjacents(self):
+        input_lines = ['abc', 'def', 'ghi']
+        grid = parse_coordinate_grid(input_lines)
+        self.assert_equal_any_order(get_adjacents(grid, (0, 0)), ['b', 'd', 'e'])
+        self.assert_equal_any_order(get_adjacents(grid, (1, 0)), ['a', 'c', 'd', 'e', 'f'])
+        self.assert_equal_any_order(get_adjacents(grid, (2, 0)), ['b', 'e', 'f'])
+
+        self.assert_equal_any_order(get_adjacents(grid, (0, 1)), ['a', 'b', 'e', 'g', 'h'])
+        self.assert_equal_any_order(get_adjacents(grid, (1, 1)), ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i'])
+        self.assert_equal_any_order(get_adjacents(grid, (2, 1)), ['b', 'c', 'e', 'h', 'i'])
+
+        self.assert_equal_any_order(get_adjacents(grid, (0, 2)), ['d', 'e', 'h'])
+        self.assert_equal_any_order(get_adjacents(grid, (1, 2)), ['d', 'e', 'f', 'g', 'i'])
+        self.assert_equal_any_order(get_adjacents(grid, (2, 2)), ['e', 'f', 'h'])
+
+    def assert_equal_any_order(self, list_a, list_b):
+        self.assertEqual(sorted(list_a), sorted(list_b))
+
 
 if __name__ == '__main__':
     unittest.main()
