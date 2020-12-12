c59c557701d70348721d6a7c768354c7982a0d89
alexburlton <alexburlton@hotmail.com>
2020-12-12 09:32:03 +0000

    Part A

--- src/day_12/day_12.py
+++ src/day_12/day_12.py
@@ -1,12 +1,34 @@
-from typing import List
+from typing import Callable, List
 
-from utils import Point, add_points
+from utils import Point, add_points, read_text_list
 
 
 class Ship:
     def __init__(self):
         self.current_position: Point = (0, 0)
         self.current_direction: Point = (1, 0)
+
+    def process_instructions(self, instructions: List[str]):
+        for instruction in instructions:
+            self.process_instruction(instruction)
+
+    def process_instruction(self, instruction: str):
+        code: str = instruction[0]
+        amount: int = int(instruction.replace(code, ''))
+        if code == 'F':
+            self.move_forward(amount)
+        elif code == 'N':
+            self.move_north(amount)
+        elif code == 'S':
+            self.move_south(amount)
+        elif code == 'E':
+            self.move_east(amount)
+        elif code == 'W':
+            self.move_west(amount)
+        elif code == 'R':
+            self.turn_by_degrees(amount, self.turn_right)
+        elif code == 'L':
+            self.turn_by_degrees(amount, self.turn_left)
 
     def move_forward(self, amount: int):
         vector: Point = (amount * self.current_direction[0], amount * self.current_direction[1])
@@ -24,6 +46,11 @@ class Ship:
     def move_west(self, amount: int):
         self.current_position = add_points(self.current_position, (-amount, 0))
 
+    def turn_by_degrees(self, degrees: int, turn_fn: Callable[[], None]):
+        turns = int(degrees / 90)
+        for i in range(0, turns):
+            turn_fn()
+
     def turn_right(self):
         self.current_direction = (self.current_direction[1], -self.current_direction[0])
 
@@ -32,3 +59,11 @@ class Ship:
 
     def manhatten_distance(self) -> int:
         return abs(self.current_position[0]) + abs(self.current_position[1])
+
+
+if __name__ == '__main__':
+    lines = read_text_list('day_12.txt')
+    ship: Ship = Ship()
+    ship.process_instructions(lines)
+
+    print(ship.manhatten_distance())
--- src/day_12/day_12.test.py
+++ src/day_12/day_12.test.py
@@ -62,6 +62,53 @@ class TestDay12(unittest.TestCase):
         ship.current_position = (17, -8)
         self.assertEqual(ship.manhatten_distance(), 25)
 
+    def test_process_instruction_f(self):
+        ship: Ship = Ship()
+        ship.process_instruction("F10")
+        self.assertEqual(ship.current_position, (10, 0))
+
+    def test_process_instruction_e(self):
+        ship: Ship = Ship()
+        ship.process_instruction("E5")
+        self.assertEqual(ship.current_position, (5, 0))
+
+    def test_process_instruction_w(self):
+        ship: Ship = Ship()
+        ship.process_instruction("W5")
+        self.assertEqual(ship.current_position, (-5, 0))
+
+    def test_process_instruction_n(self):
+        ship: Ship = Ship()
+        ship.process_instruction("N5")
+        self.assertEqual(ship.current_position, (0, 5))
+
+    def test_process_instruction_s(self):
+        ship: Ship = Ship()
+        ship.process_instruction("S5")
+        self.assertEqual(ship.current_position, (0, -5))
+
+    def test_process_instruction_r(self):
+        ship: Ship = Ship()
+        ship.process_instruction("R180")
+        self.assertEqual(ship.current_direction, (-1, 0))  # west
+        ship.process_instruction("R90")
+        self.assertEqual(ship.current_direction, (0, 1))  # north
+
+    def test_process_instruction_l(self):
+        ship: Ship = Ship()
+        ship.process_instruction("L270")
+        self.assertEqual(ship.current_direction, (0, -1))  # south
+        ship.process_instruction("L90")
+        self.assertEqual(ship.current_direction, (1, 0))  # east
+
+    def test_process_instructions(self):
+        instructions = ['F10', 'N3', 'F7', 'R90', 'F11']
+        ship: Ship = Ship()
+        ship.process_instructions(instructions)
+        self.assertEqual(ship.current_position, (17, -8))
+        self.assertEqual(ship.current_direction, (0, -1))  # south
+        self.assertEqual(ship.manhatten_distance(), 25)
+
 
 if __name__ == '__main__':
     unittest.main()
