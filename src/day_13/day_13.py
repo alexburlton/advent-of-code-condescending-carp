20d2929aa9064576075adfe785bd6c95113b42b6
alexburlton <alexburlton@hotmail.com>
2020-12-13 13:26:34 +0000

    generic solution w/tests

--- src/day_13/day_13.py
+++ src/day_13/day_13.py
@@ -6,32 +6,38 @@ import numpy
 from utils import read_text_list
 
 
+def part_b(bus_times: str) -> int:
+    raw_bus_times = bus_times.split(',')
+    buses_with_offset = [(int(raw_bus_times[i]), i) for i in range(0, len(raw_bus_times)) if raw_bus_times[i] != 'x']
+    buses_with_offset = list(reversed(sorted(buses_with_offset, key=lambda item: item[0])))
+
+    candidate = 1
+    amount_to_add = 1
+    bus, offset = buses_with_offset.pop(0)
+
+    found = False
+    while not found:
+        if (candidate + offset) % bus == 0:
+            if len(buses_with_offset) == 0:
+                found = True
+            else:
+                amount_to_add *= bus
+                bus, offset = buses_with_offset.pop(0)
+        else:
+            candidate += amount_to_add
+
+    return candidate
+
+
+def part_a(input_lines: List[str]) -> int:
+    arrival_time = int(input_lines[0])
+    buses: List[int] = [int(bus) for bus in input_lines[1].replace(',x', '').split(',')]
+    remainders = [(bus, math.ceil(arrival_time / bus) * bus - arrival_time) for bus in buses]
+    best_bus = min(remainders, key=lambda bus_and_wait: bus_and_wait[1])
+    return best_bus[0] * best_bus[1]
+
+
 if __name__ == '__main__':
     lines = read_text_list('day_13.txt')
-    arrival_time = int(lines[0])
-    buses: List[int] = [int(bus) for bus in lines[1].replace(',x', '').split(',')]
-
-    print(arrival_time)
-    print(buses)
-
-    remainders = [(bus, math.ceil(arrival_time/bus) * bus - arrival_time) for bus in buses]
-    print(remainders)
-
-    raw_bus_times = lines[1].split(',')
-    print(raw_bus_times)
-
-    noticed_factors = [983, 17, 23, 37]
-    product = numpy.product(noticed_factors)
-    added_number = 17
-
-    id_with_offset = [(int(raw_bus_times[i]), i) for i in range(0, len(raw_bus_times)) if raw_bus_times[i] != 'x']
-    id_with_offset = [(id_and_offset[0], id_and_offset[1] - added_number) for id_and_offset in id_with_offset if id_and_offset[0] not in noticed_factors]
-    print(id_with_offset)
-
-    starting_list = range(product, 1000000000000000, product)
-    print(len(starting_list))
-    for (bus, offset) in id_with_offset:
-        starting_list = [i for i in starting_list if (i + offset) % bus == 0]
-        print(len(starting_list))
-
-    print(starting_list[0] - added_number)
+    print(part_a(lines))
+    print(part_b(lines[1]))
