3fb377f78b404b3986ffce1b13d5bc3a3cb4c7aa
alexburlton <alexburlton@hotmail.com>
2020-12-16 22:13:03 +0000

    Part B

--- src/day_16/day_16.py
+++ src/day_16/day_16.py
@@ -1,9 +1,25 @@
+import math
 import re
+from dataclasses import dataclass
+from itertools import permutations
 from typing import List
 
-from utils import read_text_groups, count_where, flatten
+import numpy
 
-FieldRules = dict[str, List[range]]
+from utils import read_text_groups, flatten
+
+
+@dataclass
+class FieldRule:
+    def __init__(self, range_one: range, range_two: range):
+        self.range_one = range_one
+        self.range_two = range_two
+
+    def is_valid_value(self, value: int) -> bool:
+        return value in self.range_one or value in self.range_two
+
+
+FieldRules = dict[str, FieldRule]
 Ticket = List[int]
 
 
@@ -31,7 +47,7 @@ def parse_rule_and_add(rule: str, rules: FieldRules):
     rule_name = result.group(1)
     range_one = range(int(result.group(2)), int(result.group(3)) + 1)
     range_two = range(int(result.group(4)), int(result.group(5)) + 1)
-    rules[rule_name] = [range_one, range_two]
+    rules[rule_name] = FieldRule(range_one, range_two)
 
 
 def get_ticket_error_rate(tickets: List[Ticket], rules: FieldRules) -> int:
@@ -43,11 +59,68 @@ def sum_invalid_fields(ticket: Ticket, rules: FieldRules) -> int:
 
 
 def is_invalid_field(field: int, rules: FieldRules) -> bool:
-    all_ranges = flatten(list(rules.values()))
-    possible_ranges = [r for r in all_ranges if field in r]
-    return len(possible_ranges) == 0
+    all_rules = rules.values()
+    possible_rules = [r for r in all_rules if r.is_valid_value(field)]
+    return len(possible_rules) == 0
+
+
+def is_valid_ticket(ticket: Ticket, rules: FieldRules) -> bool:
+    return all(not is_invalid_field(field, rules) for field in ticket)
+
+
+def get_field_order(tickets: List[Ticket], rules: FieldRules) -> dict[str, int]:
+    possible_positions = list(range(0, len(rules)))
+    possible_field_positions = {field: possible_positions for field in rules.keys()}
+    for ticket in tickets:
+        knock_out_invalid_positions(possible_field_positions, ticket, rules)
+
+    resolved = resolve_positions(possible_field_positions)
+    return resolved
+
+
+def knock_out_invalid_positions(possible_positions: dict[str, List[int]], ticket: Ticket, rules: FieldRules):
+    for field, possible_so_far in possible_positions.items():
+        rule = rules[field]
+        updated = list(filter(lambda pos: rule.is_valid_value(ticket[pos]), possible_so_far))
+
+        possible_positions[field] = updated
+
+
+def resolve_positions(possible_field_positions: dict[str, List[int]]) -> dict[str, int]:
+    resolved_positions: dict[str, int] = {}
+    while len(possible_field_positions) > 0:
+        determined_fields = list(filter(lambda e: len(e[1]) == 1, possible_field_positions.items()))
+        for field, positions in determined_fields:
+            position: int = possible_field_positions.pop(field)[0]
+            resolved_positions[field] = position
+            possible_field_positions = remove_from_all_values(possible_field_positions, position)
+
+    return resolved_positions
+
+
+def remove_from_all_values(possible_field_positions: dict[str, List[int]], position: int) -> dict[str, List[int]]:
+    return {field: remove_value(positions, position) for field, positions in possible_field_positions.items()}
+
+
+def remove_value(positions: List[int], position: int) -> List[int]:
+    return list(filter(lambda pos: pos != position, positions))
+
+
+def is_valid_field_order(tickets: List[Ticket], field_order: List[FieldRule]) -> bool:
+    return all(is_valid_field_order_for_ticket(ticket, field_order) for ticket in tickets)
+
+
+def is_valid_field_order_for_ticket(ticket: Ticket, field_order: List[FieldRule]) -> bool:
+    zipped = zip(ticket, field_order)
+    valid_positions = [rule for ticket_value, rule in zipped if rule.is_valid_value(ticket_value)]
+    return len(valid_positions) == len(ticket)
 
 
 if __name__ == '__main__':
-    rules, my_ticket, other_tickets = parse_input('day_16.txt')
-    print(get_ticket_error_rate(other_tickets, rules))
+    field_rules, my_ticket, other_tickets = parse_input('day_16.txt')
+    print(get_ticket_error_rate(other_tickets, field_rules))
+    valid_other_tickets: List[Ticket] = list(filter(lambda ticket: is_valid_ticket(ticket, field_rules), other_tickets))
+    order = get_field_order(valid_other_tickets, field_rules)
+    departure_only = list(filter(lambda e: 'departure' in e[0], order.items()))
+    my_ticket_values = [my_ticket[pos] for _, pos in departure_only]
+    print(math.prod(my_ticket_values))
--- src/day_16/day_16.test.py
+++ src/day_16/day_16.test.py
@@ -8,23 +8,23 @@ class TestDay16(unittest.TestCase):
         rule_dict = {}
         parse_rule_and_add('class: 1-3 or 5-7', rule_dict)
         parse_rule_and_add('departure location: 26-715 or 727-972', rule_dict)
-        self.assertEqual(rule_dict, {'class': [range(1, 4), range(5, 8)],
-                                     'departure location': [range(26, 716), range(727, 973)]})
+        self.assertEqual(rule_dict, {'class': FieldRule(range(1, 4), range(5, 8)),
+                                     'departure location': FieldRule(range(26, 716), range(727, 973))})
 
     def test_parse_rules(self):
         rules: List[str] = ['class: 1-3 or 5-7', 'departure location: 26-715 or 727-972']
         rule_dict = parse_rules(rules)
-        self.assertEqual(rule_dict, {'class': [range(1, 4), range(5, 8)],
-                                     'departure location': [range(26, 716), range(727, 973)]})
+        self.assertEqual(rule_dict, {'class': FieldRule(range(1, 4), range(5, 8)),
+                                     'departure location': FieldRule(range(26, 716), range(727, 973))})
 
     def test_parse_ticket(self):
         self.assertEqual(parse_ticket('50,67,100,73'), [50, 67, 100, 73])
 
     def test_parse_input(self):
         rules, my_ticket, other_tickets = parse_input('day_16_example.txt')
-        self.assertEqual(rules, {'class': [range(1, 4), range(5, 8)],
-                                 'row': [range(6, 12), range(33, 45)],
-                                 'seat': [range(13, 41), range(45, 51)]})
+        self.assertEqual(rules, {'class': FieldRule(range(1, 4), range(5, 8)),
+                                 'row': FieldRule(range(6, 12), range(33, 45)),
+                                 'seat': FieldRule(range(13, 41), range(45, 51))})
         self.assertEqual(my_ticket, [7, 1, 14])
         self.assertEqual(other_tickets, [[7, 3, 47], [40, 4, 50], [55, 2, 20], [38, 6, 12]])
 
@@ -37,16 +37,55 @@ class TestDay16(unittest.TestCase):
         for field in [7, 3, 47, 40, 50, 2, 20, 38, 6]:
             self.assertFalse(is_invalid_field(field, rules))
 
-    def test_is_invalid_ticket(self):
+    def test_sum_invalid_fields(self):
         rules, _, _ = parse_input('day_16_example.txt')
         self.assertEqual(sum_invalid_fields([7, 3, 47], rules), 0)
         self.assertEqual(sum_invalid_fields([40, 4, 50], rules), 4)
         self.assertEqual(sum_invalid_fields([55, 2, 20], rules), 55)
         self.assertEqual(sum_invalid_fields([38, 6, 12], rules), 12)
 
+    def test_is_valid_ticket(self):
+        rules, _, _ = parse_input('day_16_example.txt')
+        self.assertTrue(is_valid_ticket([7, 3, 47], rules))
+        self.assertFalse(is_valid_ticket([40, 4, 50], rules))
+        self.assertFalse(is_valid_ticket([55, 2, 20], rules))
+        self.assertFalse(is_valid_ticket([38, 6, 12], rules))
+        # grr, stupid edge case
+        self.assertFalse(is_valid_ticket([0, 3, 47], rules))
+
     def test_get_ticket_error_rate(self):
         rules, _, other_tickets = parse_input('day_16_example.txt')
         self.assertEqual(get_ticket_error_rate(other_tickets, rules), 71)
+
+    def test_is_valid_field_order(self):
+        class_field = FieldRule(range(0, 2), range(4, 20))
+        row_field = FieldRule(range(0, 6), range(8, 20))
+        seat_field = FieldRule(range(0, 14), range(16, 20))
+
+        tickets = [[3, 9, 18], [15, 1, 5], [5, 14, 9]]
+
+        self.assertTrue(is_valid_field_order(tickets, [row_field, class_field, seat_field]))
+        self.assertFalse(is_valid_field_order(tickets, [row_field, seat_field, class_field]))
+        self.assertFalse(is_valid_field_order(tickets, [class_field, row_field, seat_field]))
+        self.assertFalse(is_valid_field_order(tickets, [class_field, seat_field, row_field]))
+        self.assertFalse(is_valid_field_order(tickets, [seat_field, class_field, row_field]))
+        self.assertFalse(is_valid_field_order(tickets, [seat_field, row_field, class_field]))
+
+    def test_get_field_order(self):
+        rules, _, tickets = parse_input('day_16_example_b.txt')
+        self.assertEqual(get_field_order(tickets, rules), {'class': 1, 'row': 0, 'seat': 2})
+
+    def test_remove_value(self):
+        self.assertEqual(remove_value([1, 3, 10], 3), [1, 10])
+
+    def test_remove_from_all_values(self):
+        some_map = {'foo': [1, 3, 5], 'bar': [1, 7, 10, 15], 'baz': [3, 5, 10]}
+        self.assertEqual(remove_from_all_values(some_map, 3), {'foo': [1, 5], 'bar': [1, 7, 10, 15], 'baz': [5, 10]})
+
+    def test_resolve_positions(self):
+        possible_positions = {'foo': [1, 3, 10], 'bar': [1, 2, 10], 'baz': [10, 3], 'buzz': [3]}
+        resolved = resolve_positions(possible_positions)
+        self.assertEqual(resolved, {'foo': 1, 'bar': 2, 'baz': 10, 'buzz': 3})
 
 
 if __name__ == '__main__':
