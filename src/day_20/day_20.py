d40abdbecec017f96f9aeb7a3a5ca9d3789e5457
Alex <alex.burlton@sonocent.com>
2020-12-20 10:55:27 +0000

    Part A

--- src/day_20/day_20.py
+++ src/day_20/day_20.py
@@ -1,5 +1,6 @@
+import math
 import re
-from typing import List, Callable
+from typing import List, Callable, Set
 
 from utils import Grid, parse_coordinate_grid, Point, read_text_groups
 
@@ -9,37 +10,70 @@ class Tile(object):
     id: int
     width: int
 
-    def __init__(self, id: int, grid_lines: List[str]):
+    def __init__(self, id: int, points: Grid):
         self.id = id
-        self.points = parse_coordinate_grid(grid_lines)
-        self.width = len(grid_lines)
+        self.points = points
+        self.width = int(math.sqrt(len(points)))
 
-    def rotate_right(self):
-        self.transform(lambda point: (self.width - 1 - point[1], point[0]))
+    @classmethod
+    def from_lines(cls, id: int, grid_lines: List[str]):
+        grid = parse_coordinate_grid(grid_lines)
+        return cls(id, grid)
 
-    def flip_horizontal(self):
-        self.transform(lambda point: (self.width - 1 - point[0], point[1]))
+    def rotate_right(self) -> 'Tile':
+        return self.transform(lambda point: (self.width - 1 - point[1], point[0]))
 
-    def flip_vertical(self):
-        self.transform(lambda point: (point[0], self.width - 1 - point[1]))
+    def flip_horizontal(self) -> 'Tile':
+        return self.transform(lambda point: (self.width - 1 - point[0], point[1]))
 
-    def transform(self, point_transformer: Callable[[Point], Point]):
+    def flip_vertical(self) -> 'Tile':
+        return self.transform(lambda point: (point[0], self.width - 1 - point[1]))
+
+    def transform(self, point_transformer: Callable[[Point], Point]) -> 'Tile':
         new_points = Grid()
         for point, value in self.points.items():
             new_point: Point = point_transformer(point)
             new_points[new_point] = value
 
-        self.points = new_points
+        return Tile(self.id, new_points)
+
+    def get_edges(self) -> Set[str]:
+        sorted_points: List[Point] = sorted(self.points.keys())
+        return {''.join([self.points[point] for point in sorted_points if point[0] == 0]),
+                ''.join([self.points[point] for point in sorted_points if point[0] == self.width - 1]),
+                ''.join([self.points[point] for point in sorted_points if point[1] == 0]),
+                ''.join([self.points[point] for point in sorted_points if point[1] == self.width - 1])}
+
+    def get_all_possible_edges(self) -> Set[str]:
+        current_edges = self.get_edges()
+        flipped_edges = {edge[::-1] for edge in current_edges}
+        return current_edges.union(flipped_edges)
 
 
 def parse_tile(tile_lines: List[str]) -> Tile:
     id_line = tile_lines.pop(0)
     re_match = re.match(r'^Tile ([\d]+):$', id_line)
     id: int = int(re_match.group(1))
-    return Tile(id, tile_lines)
+    return Tile.from_lines(id, tile_lines)
+
+
+def find_corners(tiles: List[Tile]) -> List[int]:
+    tile_to_matches: dict[int, int] = {}
+    for tile in tiles:
+        matches: int = 0
+        my_edges = tile.get_all_possible_edges()
+        other_tiles = [other_tile for other_tile in tiles if other_tile.get_all_possible_edges() != tile.get_all_possible_edges()]
+        for other_tile in other_tiles:
+            other_edges = other_tile.get_all_possible_edges()
+            intersection = other_edges.intersection(my_edges)
+            matches += len(intersection)
+        tile_to_matches[tile.id] = matches
+
+    return [key for key, value in tile_to_matches.items() if value == 4]
 
 
 if __name__ == '__main__':
     tile_groups = read_text_groups('day_20.txt')
     tiles: List[Tile] = [parse_tile(group.splitlines()) for group in tile_groups]
     print(len(tiles))
+    print(math.prod(find_corners(tiles)))
--- src/day_20/day_20.test.py
+++ src/day_20/day_20.test.py
@@ -5,7 +5,7 @@ from src.day_20.day_20 import *
 
 class TestDay19(unittest.TestCase):
     def test_construct_tile(self):
-        tile: Tile = Tile(3, ['abc', 'def', 'ghi'])
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
         self.assertEqual(tile.id, 3)
         self.assertEqual(tile.width, 3)
         self.assertEqual(tile.points, {(0, 0): 'a', (1, 0): 'b', (2, 0): 'c',
@@ -21,28 +21,64 @@ class TestDay19(unittest.TestCase):
                                        (0, 2): 'g', (1, 2): 'h', (2, 2): 'i'})
 
     def test_rotate_tile_right(self):
-        tile: Tile = Tile(3, ['abc', 'def', 'ghi'])
-        tile.rotate_right()
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        rotated = tile.rotate_right()
 
-        self.assertEqual(tile.points, {(0, 0): 'g', (1, 0): 'd', (2, 0): 'a',
-                                       (0, 1): 'h', (1, 1): 'e', (2, 1): 'b',
-                                       (0, 2): 'i', (1, 2): 'f', (2, 2): 'c'})
+        self.assertEqual(rotated.points, {(0, 0): 'g', (1, 0): 'd', (2, 0): 'a',
+                                          (0, 1): 'h', (1, 1): 'e', (2, 1): 'b',
+                                          (0, 2): 'i', (1, 2): 'f', (2, 2): 'c'})
 
     def test_flip_tile_horizontal(self):
-        tile: Tile = Tile(3, ['abc', 'def', 'ghi'])
-        tile.flip_horizontal()
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        flipped = tile.flip_horizontal()
 
-        self.assertEqual(tile.points, {(0, 0): 'c', (1, 0): 'b', (2, 0): 'a',
-                                       (0, 1): 'f', (1, 1): 'e', (2, 1): 'd',
-                                       (0, 2): 'i', (1, 2): 'h', (2, 2): 'g'})
+        self.assertEqual(flipped.points, {(0, 0): 'c', (1, 0): 'b', (2, 0): 'a',
+                                          (0, 1): 'f', (1, 1): 'e', (2, 1): 'd',
+                                          (0, 2): 'i', (1, 2): 'h', (2, 2): 'g'})
 
     def test_flip_tile_vertical(self):
-        tile: Tile = Tile(3, ['abc', 'def', 'ghi'])
-        tile.flip_vertical()
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        flipped = tile.flip_vertical()
 
-        self.assertEqual(tile.points, {(0, 0): 'g', (1, 0): 'h', (2, 0): 'i',
-                                       (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
-                                       (0, 2): 'a', (1, 2): 'b', (2, 2): 'c'})
+        self.assertEqual(flipped.points, {(0, 0): 'g', (1, 0): 'h', (2, 0): 'i',
+                                          (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
+                                          (0, 2): 'a', (1, 2): 'b', (2, 2): 'c'})
+
+    def test_get_edges(self):
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        edges = tile.get_edges()
+        self.assertEqual(edges, {'abc', 'adg', 'cfi', 'ghi'})
+
+    def test_get_edges_after_rotation(self):
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        rotated = tile.rotate_right()
+        edges = rotated.get_edges()
+        self.assertEqual(edges, {'abc', 'gda', 'ifc', 'ghi'})
+
+    def test_get_edges_after_vertical_flip(self):
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        flipped = tile.flip_vertical()
+        edges = flipped.get_edges()
+        self.assertEqual(edges, {'abc', 'gda', 'ifc', 'ghi'})
+
+    def test_get_edges_after_horizontal_flip(self):
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        flipped = tile.flip_horizontal()
+        edges = flipped.get_edges()
+        self.assertEqual(edges, {'cba', 'cfi', 'ihg', 'adg'})
+
+    def test_get_all_possible_edges(self):
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        edges = tile.get_all_possible_edges()
+        self.assertEqual(edges, {'abc', 'cba', 'adg', 'gda', 'ghi', 'ihg', 'cfi', 'ifc'})
+
+    def test_find_edge_tiles(self):
+        tile_groups = read_text_groups('day_20_example.txt')
+        tiles: List[Tile] = [parse_tile(group.splitlines()) for group in tile_groups]
+        self.assert_equal_any_order(find_corners(tiles), [1951, 3079, 2971, 1171])
+
+    def assert_equal_any_order(self, list_a, list_b):
+        self.assertEqual(sorted(list_a), sorted(list_b))
 
 
 if __name__ == '__main__':
