d1967696b48aa935a4fd71ae0994b44ebdce66e0
Alex <alex.burlton@sonocent.com>
2020-12-20 11:39:15 +0000

    Some prep for Part B. Properly classify corners, edges and inner pieces.

--- src/day_20/day_20.py
+++ src/day_20/day_20.py
@@ -6,14 +6,17 @@ from utils import Grid, parse_coordinate_grid, Point, read_text_groups
 
 
 class Tile(object):
-    points: Grid
+    grid: Grid
     id: int
     width: int
+    sorted_points: List[Point] = []
+    matched_edges: Set[str] = set()
 
-    def __init__(self, id: int, points: Grid):
+    def __init__(self, id: int, grid: Grid):
         self.id = id
-        self.points = points
-        self.width = int(math.sqrt(len(points)))
+        self.grid = grid
+        self.width = int(math.sqrt(len(grid)))
+        self.sorted_points = sorted(grid.keys())
 
     @classmethod
     def from_lines(cls, id: int, grid_lines: List[str]):
@@ -31,23 +34,47 @@ class Tile(object):
 
     def transform(self, point_transformer: Callable[[Point], Point]) -> 'Tile':
         new_points = Grid()
-        for point, value in self.points.items():
+        for point, value in self.grid.items():
             new_point: Point = point_transformer(point)
             new_points[new_point] = value
 
         return Tile(self.id, new_points)
 
+    def top_edge(self) -> str:
+        return ''.join([self.grid[point] for point in self.sorted_points if point[1] == 0])
+
+    def bottom_edge(self) -> str:
+        return ''.join([self.grid[point] for point in self.sorted_points if point[1] == self.width - 1])
+
+    def left_edge(self) -> str:
+        return ''.join([self.grid[point] for point in self.sorted_points if point[0] == 0])
+
+    def right_edge(self) -> str:
+        return ''.join([self.grid[point] for point in self.sorted_points if point[0] == self.width - 1])
+
     def get_edges(self) -> Set[str]:
-        sorted_points: List[Point] = sorted(self.points.keys())
-        return {''.join([self.points[point] for point in sorted_points if point[0] == 0]),
-                ''.join([self.points[point] for point in sorted_points if point[0] == self.width - 1]),
-                ''.join([self.points[point] for point in sorted_points if point[1] == 0]),
-                ''.join([self.points[point] for point in sorted_points if point[1] == self.width - 1])}
+        return {self.top_edge(), self.bottom_edge(), self.left_edge(), self.right_edge()}
+
+    def update_matched_edges(self, matched_edges: Set[str]):
+        self.matched_edges = matched_edges.intersection(self.get_edges())
 
     def get_all_possible_edges(self) -> Set[str]:
         current_edges = self.get_edges()
         flipped_edges = {edge[::-1] for edge in current_edges}
         return current_edges.union(flipped_edges)
+
+
+PuzzleGrid = dict[Point, Tile]
+
+
+class Puzzle(object):
+    grid: PuzzleGrid
+
+    def __init__(self):
+        self.grid = PuzzleGrid()
+
+    def add_tile(self, point: Point, tile: Tile):
+        self.grid[point] = tile
 
 
 def parse_tile(tile_lines: List[str]) -> Tile:
@@ -57,23 +84,36 @@ def parse_tile(tile_lines: List[str]) -> Tile:
     return Tile.from_lines(id, tile_lines)
 
 
-def find_corners(tiles: List[Tile]) -> List[int]:
-    tile_to_matches: dict[int, int] = {}
+def classify_tiles(tiles: List[Tile]) -> tuple[List[Tile], List[Tile], List[Tile]]:
+    corners: List[Tile] = []
+    edges: List[Tile] = []
+    inners: List[Tile] = []
     for tile in tiles:
-        matches: int = 0
+        matches: Set[str] = set()
         my_edges = tile.get_all_possible_edges()
         other_tiles = [other_tile for other_tile in tiles if other_tile.get_all_possible_edges() != tile.get_all_possible_edges()]
         for other_tile in other_tiles:
             other_edges = other_tile.get_all_possible_edges()
             intersection = other_edges.intersection(my_edges)
-            matches += len(intersection)
-        tile_to_matches[tile.id] = matches
+            matches = matches.union(intersection)
+        tile.update_matched_edges(matches)
+        if len(matches) == 4:
+            corners.append(tile)
+        elif len(matches) == 6:
+            edges.append(tile)
+        else:
+            inners.append(tile)
 
-    return [key for key, value in tile_to_matches.items() if value == 4]
+    return corners, edges, inners
+
+
+# def make_full_puzzle(corners: List[Tile], edges: List[Tile], inners: List[Tile]) -> Puzzle:
+
 
 
 if __name__ == '__main__':
     tile_groups = read_text_groups('day_20.txt')
     tiles: List[Tile] = [parse_tile(group.splitlines()) for group in tile_groups]
     print(len(tiles))
-    print(math.prod(find_corners(tiles)))
+    corners, edges, inners = classify_tiles(tiles)
+    print(math.prod([corner.id for corner in corners]))
--- src/day_20/day_20.test.py
+++ src/day_20/day_20.test.py
@@ -8,46 +8,50 @@ class TestDay19(unittest.TestCase):
         tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
         self.assertEqual(tile.id, 3)
         self.assertEqual(tile.width, 3)
-        self.assertEqual(tile.points, {(0, 0): 'a', (1, 0): 'b', (2, 0): 'c',
-                                       (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
-                                       (0, 2): 'g', (1, 2): 'h', (2, 2): 'i'})
+        self.assertEqual(tile.grid, {(0, 0): 'a', (1, 0): 'b', (2, 0): 'c',
+                                     (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
+                                     (0, 2): 'g', (1, 2): 'h', (2, 2): 'i'})
 
     def test_parse_tile(self):
         tile_lines = ['Tile 2843:', 'abc', 'def', 'ghi']
         tile: Tile = parse_tile(tile_lines)
         self.assertEqual(tile.id, 2843)
-        self.assertEqual(tile.points, {(0, 0): 'a', (1, 0): 'b', (2, 0): 'c',
-                                       (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
-                                       (0, 2): 'g', (1, 2): 'h', (2, 2): 'i'})
+        self.assertEqual(tile.grid, {(0, 0): 'a', (1, 0): 'b', (2, 0): 'c',
+                                     (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
+                                     (0, 2): 'g', (1, 2): 'h', (2, 2): 'i'})
 
     def test_rotate_tile_right(self):
         tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
         rotated = tile.rotate_right()
 
-        self.assertEqual(rotated.points, {(0, 0): 'g', (1, 0): 'd', (2, 0): 'a',
-                                          (0, 1): 'h', (1, 1): 'e', (2, 1): 'b',
-                                          (0, 2): 'i', (1, 2): 'f', (2, 2): 'c'})
+        self.assertEqual(rotated.grid, {(0, 0): 'g', (1, 0): 'd', (2, 0): 'a',
+                                        (0, 1): 'h', (1, 1): 'e', (2, 1): 'b',
+                                        (0, 2): 'i', (1, 2): 'f', (2, 2): 'c'})
 
     def test_flip_tile_horizontal(self):
         tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
         flipped = tile.flip_horizontal()
 
-        self.assertEqual(flipped.points, {(0, 0): 'c', (1, 0): 'b', (2, 0): 'a',
-                                          (0, 1): 'f', (1, 1): 'e', (2, 1): 'd',
-                                          (0, 2): 'i', (1, 2): 'h', (2, 2): 'g'})
+        self.assertEqual(flipped.grid, {(0, 0): 'c', (1, 0): 'b', (2, 0): 'a',
+                                        (0, 1): 'f', (1, 1): 'e', (2, 1): 'd',
+                                        (0, 2): 'i', (1, 2): 'h', (2, 2): 'g'})
 
     def test_flip_tile_vertical(self):
         tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
         flipped = tile.flip_vertical()
 
-        self.assertEqual(flipped.points, {(0, 0): 'g', (1, 0): 'h', (2, 0): 'i',
-                                          (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
-                                          (0, 2): 'a', (1, 2): 'b', (2, 2): 'c'})
+        self.assertEqual(flipped.grid, {(0, 0): 'g', (1, 0): 'h', (2, 0): 'i',
+                                        (0, 1): 'd', (1, 1): 'e', (2, 1): 'f',
+                                        (0, 2): 'a', (1, 2): 'b', (2, 2): 'c'})
 
     def test_get_edges(self):
         tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
         edges = tile.get_edges()
         self.assertEqual(edges, {'abc', 'adg', 'cfi', 'ghi'})
+        self.assertEqual(tile.top_edge(), 'abc')
+        self.assertEqual(tile.bottom_edge(), 'ghi')
+        self.assertEqual(tile.left_edge(), 'adg')
+        self.assertEqual(tile.right_edge(), 'cfi')
 
     def test_get_edges_after_rotation(self):
         tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
@@ -72,10 +76,18 @@ class TestDay19(unittest.TestCase):
         edges = tile.get_all_possible_edges()
         self.assertEqual(edges, {'abc', 'cba', 'adg', 'gda', 'ghi', 'ihg', 'cfi', 'ifc'})
 
-    def test_find_edge_tiles(self):
+    def test_classify_tiles(self):
         tile_groups = read_text_groups('day_20_example.txt')
         tiles: List[Tile] = [parse_tile(group.splitlines()) for group in tile_groups]
-        self.assert_equal_any_order(find_corners(tiles), [1951, 3079, 2971, 1171])
+        corners, edges, inners = classify_tiles(tiles)
+        self.assert_equal_any_order([corner.id for corner in corners], [1951, 3079, 2971, 1171])
+        self.assert_equal_any_order([edge.id for edge in edges], [2311, 2729, 2473, 1489])
+        self.assert_equal_any_order([inner.id for inner in inners], [1427])
+
+    def test_update_matched_edges(self):
+        tile: Tile = Tile.from_lines(3, ['abc', 'def', 'ghi'])
+        tile.update_matched_edges({'abc', 'cba'})
+        self.assertEqual(tile.matched_edges, {'abc'})
 
     def assert_equal_any_order(self, list_a, list_b):
         self.assertEqual(sorted(list_a), sorted(list_b))
